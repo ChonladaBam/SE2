fd481c762370533d9c97e7e7b19666d1c2228b94
ChonladaBam <stu6410014115@sskru.ac.th>
2022-12-18 22:17:49 +0700

    week3

---
+++ file jawascript.js
@@ -0,0 +1,46 @@
+var name = "ข้อความ";
+var number = 9;
+var array = [1,2,3];
+a[0] = 5;
+
+let nametwo = "ชลลดา"
+var object = {"size":10,"name":"dang"};
+console.log(object.name); // output:"dang"
+console.log(object.size); //10;
+console.log (name,number,array,nametwo,object);
+object.size = "arai"
+
+if (nametwo===namethree)
+
+name = 10
+console.logg(name);
+var st = 10
+console.log(parsient(st) + 8); // output "108"
+st =10;
+console.log(st +8 ); // output 18
+//
+//}
+function functionname(paraml,param2)
+{
+
+}
+var f = function()
+{
+     console.log ("มีการเรียกฟังก์ชัน");    
+}
+f();
+ if (x>y)
+ {console.log("x>y");
+}
+else if ("x<y");
+{
+    console.log("x<y");
+}
+else
+{
+    console.log ("x>y");
+}
+for(var i = 10; i>0; 1--)
+{
+    console.log(i);
+}
